3b350ca2b6adfb22e6b0d67751e58b480d1bcdbe
tiangebi0222 <60284176+tiangebi0222@users.noreply.github.com>
2020-01-25 11:50:21 -0500

    Updated quote to something much much better (IMHO)

--- molecool/functions.py
+++ molecool/functions.py
@@ -23,9 +23,9 @@ def canvas(with_attribution=True):
         Compiled string including quote and optional attribution
     """
 
-    quote = "The code is but a canvas to our imagination!!!"
+    quote = "This package is not only cool, but MOLEcool!!!"
     if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
+        quote += "\n\t- Adapted from Ben"
     return quote
 
 
